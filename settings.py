84c3ebfe6afb524f0aa74127a3d7828c57865a73
Egor Fedorov <fegor2004@gmail.com>
2020-02-19 20:54:02 +0300

    Travis, settings file

---
+++ settings.py
@@ -0,0 +1,7 @@
+version = 5.103
+folder = 'csv/'
+csv_file = 'age_research1.csv'
+analyze = True
+log_needed = True
+project_folder = "/home/fegor04/PycharmProjects/Age-Analyzer"
+neural_network_file = 'neuronet.sav'
