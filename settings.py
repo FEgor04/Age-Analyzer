8cdad7776345175c47ea76ba163266ba1b97785a
Egor Fedorov <fegor2004@gmail.com>
2020-03-23 12:12:18 +0300

    Tests refactor

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+from typing import Tuple
+
 version = 5.103
 folder = 'csv/'
 csv_file = 'age_research1.csv'
@@ -8,3 +10,4 @@ project_folder = "."
 tg_api = str(os.environ.get('tg_api'))
 token = str(os.environ.get('vk_api'))
 neural_network_file = 'neuronet.sav'
+min_version: Tuple[int, int, int] = (3, 6, 10)  # Version in which statistics._counts is present (NEEDED FOR TESTS)
