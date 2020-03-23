ccd42617d276c34ec0472ff8c3d363cdf4d6d9ca
Egor Fedorov <fegor2004@gmail.com>
2020-03-23 12:45:08 +0300

    Travis tests with python 3.7, 3.6.9, 3.6.10

--- settings.py
+++ settings.py
@@ -10,4 +10,4 @@ project_folder = "."
 tg_api = str(os.environ.get('tg_api'))
 token = str(os.environ.get('vk_api'))
 neural_network_file = 'neuronet.sav'
-min_version: Tuple[int, int, int] = (3, 6, 10)  # Version in which statistics._counts is present (NEEDED FOR TESTS)
+min_version: Tuple[int, int, int] = (3, 7, 0)  # Version in which statistics._counts is present (NEEDED FOR TESTS)
