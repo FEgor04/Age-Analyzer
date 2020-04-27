1b5e769667e93c22db43a993615b9ba3b9a28625
Egor Fedorov <fegor2004@gmail.com>
2020-04-27 15:31:45 +0300

    analyze and insert into table

--- postgres_report.py
+++ postgres_report.py
@@ -1,15 +1,76 @@
+import datetime
+import statistics as st
+
 import psycopg2
 
+import age_analyzer
+import neuroanalyzer
 import settings
 
-connection = psycopg2.connect(
-    database=settings.db_name,
-    user=settings.db_login,
-    password=settings.db_pass,
-    host=settings.db_ip,
-    port=settings.db_port
-)
+
+def create_table():
+    connection = psycopg2.connect(
+        database=settings.db_name,
+        user=settings.db_login,
+        password=settings.db_pass,
+        host=settings.db_ip,
+        port=settings.db_port
+    )
+    cur = connection.cursor()
+    cur.execute(' \
+        CREATE TABLE IF NOT EXISTS analyzed( \
+        ID integer, \
+        DOMAIN varchar(250), \
+        FIRST_NAME varchar(250), \
+        LAST_NAME varchar(250), \
+        ESTIMATED_AGE real, \
+        MEAN real, \
+        MODE real, \
+        HARMONIC_MEAN real, \
+        MEDIAN real, \
+        STD real, \
+        FRIENDS_CNT integer, \
+        VERIFIED bool, \
+        LAST_CHECK timestamp \
+    )')
+    connection.commit()
+    connection.close()
 
 
-def query():
-    pass
+def analyze_and_insert(domain, model):
+    try:
+        connection = psycopg2.connect(
+            database=settings.db_name,
+            user=settings.db_login,
+            password=settings.db_pass,
+            host=settings.db_ip,
+            port=settings.db_port
+        )
+    except:
+        print("I am unable to connect to the database")
+    # print(connection)
+    ages = age_analyzer.get_friends_ages(domain)
+    estimated_age = model.query(ages)
+    target_id = age_analyzer.get_id_by_domain(domain)
+    name = age_analyzer.get_name(domain)
+    mean = round(st.mean(ages), 2)
+    hmean = round(st.harmonic_mean(ages), 2)
+    mode = round(neuroanalyzer.find_average_mode(ages), 2)
+    median = round(st.median(ages), 2)
+    std = round(neuroanalyzer.find_std(ages), 2)
+    friends_cnt = len(ages)
+    today = datetime.datetime.now()
+    # TODO: update if there are already some info about ID
+    cur = connection.cursor()
+    # print(cur)
+    # print(name)
+    query = (f"insert into analyzed("
+             f"id, domain, first_name, last_name, estimated_age, mean, mode, harmonic_mean,"
+             f"median, std, friends_cnt, verified) values ("
+             f"{target_id}, \'{domain}\', \'{name['first_name']}\', \'{name['last_name']}\', {int(estimated_age)}, {mean}, {mode}, {hmean}, {median},"
+             f"{std}, {friends_cnt}, {False}"
+             f")")
+    print(query)
+    cur.execute(query)
+    connection.commit()
+    connection.close()
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+
 version = 5.103
 folder = 'csv/'
 csv_file = 'age_research1.csv'
@@ -6,4 +7,9 @@ log_needed = False
 project_folder = "."
 tg_api = os.environ.get('tg_api')
 token = os.environ.get('vk_api')
+db_ip = os.environ.get('db_ip')
+db_name = os.environ.get('db_name')
+db_login = os.environ.get('db_login')
+db_pass = os.environ.get('db_pass')
+db_port = os.environ.get('db_port')
 neural_network_file = 'neuronet.sav'
